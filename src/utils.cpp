ea7af1710b0624f3445f6843228539090bb132e5
agauniyal <agauniyal@outlook.com>
2016-09-17 23:24:05 +0530

    add json config file support
    plain text file config deprecated, add tests for config file read,
    add more pins to emulation

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,14 +1,21 @@
 #include "utils.hpp"
 #include <fstream>
+#include <sstream>
+#include "json.hpp"
 
 std::string gpio::getGPIOBasePath(const std::string _file)
 {
+	using json = nlohmann::json;
+
 	std::ifstream config(_file);
 	std::string path = "";
 	if (config) {
-		std::getline(config, path);
+		std::stringstream buffer;
+		buffer << config.rdbuf();
+		auto json_config = json::parse(buffer);
+		return json_config["path"];
+
 	} else {
 		return "debug/raspi/path";
 	}
-	return path;
 }
