44b756ca77c4a8e7f959cd6adcf21d15a5e94002
jeetu galav <jeetugalav@outlook.com>
2016-10-12 17:00:31 +0530

    trip wire issues fixed

--- include/device_TripWire.hpp
+++ include/device_TripWire.hpp
@@ -10,15 +10,15 @@ private:
 	std::mutex m_trip;
 	std::mutex m_lastBreak;
 	std::chrono::time_point<std::chrono::system_clock> lastBreak;
-
+	int maxcycles;
 	TripWire(const TripWire &) = delete;
 	TripWire &operator=(const TripWire &) = delete;
 
 	static const std::string TripWire_PATH;
 
 public:
-	TripWire(const unsigned int _p, const std::string _n = "", const std::string _id = "")
-	    : Device(_p, _n, _id)
+	TripWire(const unsigned int _p, const std::string _n = "", const std::string _id = "",const int _c=200)
+	    : Device(_p, _n, _id),maxcycles(_c)
 	{
 		logger->info("TripWire <{}> - pin <{}> constructed", getName(), getPowerPin());
 	}
@@ -26,10 +26,10 @@ public:
 	TripWire(const json _j, const std::string _id = "")
 	    : TripWire(_j.at("powerPin"), _j.at("name"), _id)
 	{
-		// set lastBreak if found
+		
 	}
 
-	int getResistance();
+	bool intrusion();
 	deviceType getType() const override { return deviceType::TripWire; }
 };
 }
--- src/device_TripWire.cpp
+++ src/device_TripWire.cpp
@@ -5,19 +5,23 @@ using namespace isaac;
 const std::string TripWire::TripWire_PATH = config::getGPIOBasePath();
 
 
-int TripWire::getResistance()
+bool TripWire::intrusion()
 {
-	int cycles = 0;
+	
 	if (m_trip.try_lock()) {
+		int cycles = 0;
 		Device::on();
 		Device::off();
-		// std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		
 		Device::setDirection(0);
-		int pin = getPowerPin();
-		while (read() == 0 && cycles < 2000) {
+		
+		while (read() == 0 && cycles < maxcycles) {
 			++cycles;
 		}
 		m_trip.unlock();
+	
+		return (cycles==maxcycles)?true:false;
 	}
-	return cycles;
+	return false;
 }
+
